7d3e6b2c97b425306e0719dbb37bcaa84517211e
kayleigh <kmovalli@islander.tamucc>
2020-04-20 21:39:44 -0500

    updated code

---
+++ app/src/main/java/com/example/adastra/Interpreter.java
@@ -0,0 +1,6 @@
+package com.example.adastra;
+
+class Interpreter {
+    public Interpreter(Object loadModelFile) {
+    }
+}
--- app/src/main/java/com/example/adastra/MainActivity.java
+++ app/src/main/java/com/example/adastra/MainActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.tensorflow;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 File file = new File("testmodel.tflite");
 
@@ -25,12 +30,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int timer = 0;
     Toast toast;
     Interpreter tflite;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startTime = System.currentTimeMillis();
+
+        try {
+            tflite = new Interpreter(loadModelFile());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
         final ImageButton button1 = findViewById(R.id.imageButton1);
         final ImageButton button2 = findViewById(R.id.imageButton2);
@@ -39,6 +51,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         final ImageButton button5 = findViewById(R.id.imageButton5);
         final ImageButton button6 = findViewById(R.id.imageButton6);
 
+        submit = (Button) findViewById(R.id.submitButton) ;
+
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
@@ -46,10 +60,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button5.setOnClickListener(this);
         button6.setOnClickListener(this);
 
-        try {
-            tflite = new Interpreter(loadModelFile());
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                float predicition = doInference(submit);
+                outputN
+            }
         }
 
     }
@@ -147,6 +163,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             e.printStackTrace();
         }
         return line;
+    }
+
+    public void doInference(int butt1, butt2, butt3, butt4, butt5, butt6){
+        int p1;
+        int p2;
+        int p3;
+        int p4;
+        int p5;
+        int p6;
+
+        p1 = int.valueOf(butt1);
+        p2 = int.valueOf(butt2);
+        p3 = int.valueOf(butt3);
+        p4 = int.valueOf(butt4);
+        p5 = int.valueOf(butt5);
+        p6 = int.valueOf(butt6);
+
+    }
+    private MappedByteBuffer loadModelFile() throws IOException {
+        AssetFileDescriptor fileDescriptor = this.getAssets().openFD("testmodel.tflite");
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
     }
     interpreter.close();
 }
