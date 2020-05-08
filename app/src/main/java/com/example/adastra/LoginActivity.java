50d212626843e8ee20460c8675e419e2ac9bbc2d
kmovalli <kmovalli@islander.tamucc.edu>
2020-05-07 21:37:04 -0500

    updated code

--- app/src/main/java/com/example/adastra/LoginActivity.java
+++ app/src/main/java/com/example/adastra/LoginActivity.java
@@ -1,27 +1,39 @@
 package com.example.adastra;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlogin);
 
+        login = (Button) findViewById(R.id.login) ;
+
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Users, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+         login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                openMainActivity();
+            }
+        });
     }
 
     @Override
@@ -34,4 +46,11 @@ public class LoginActivity extends AppCompatActivity implements AdapterView.OnIt
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
